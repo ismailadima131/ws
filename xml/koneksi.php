bec16a4a01af7052ed43f440ec54e5528b559a6f
ismailadima131 <ismail.adima@mail.ugm.ac.id>
2014-10-01 08:37:20 +0700

    praktikum sit 1 oktober 2014

---
+++ xml/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+	//koneksi database
+	$link = mysql_connect('localhost','root','') 
+	or die('Could not connect : '.mysql_error());
+	mysql_select_db('mahasiswa') or die ('Could not select database');
+
+?>
