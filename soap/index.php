629d710e4ad3b46ccff417940825e57b212bf9d1
ismailadima131 <ismail.adima@mail.ugm.ac.id>
2014-11-05 08:15:33 +0700

    5 November 2014

--- soap/index.php
+++ soap/index.php
@@ -1,41 +1,54 @@
 <?php
-	//memulai session
-	session_start();
-	//cek adanya session
-	if(ISSET ($_SESSION["username"])){
-	}else header("location:login.php");
+  error_reporting(E_ALL);
+  ini_set('display_error',1);
+  //memulai session
+  session_start();
+  //cek adanya session
+  if(ISSET($_SESSION['username'])){
+    //jika tidak ada session 
+  } else
+  header("location:login.php");
 ?>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="asset/css/navbar-fixed-top.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="asset/js/bootstrap.js"> </script> 
 <body>
-	<div class="navbar navbar fixed-top">
-		<div class="navbar-inner">
-			<div class="row">
-				<div class="span2">
-					<a class="brand" href="#">&nbsp; &nbsp E-Learning</a>
-				</div>
-				<div class="nav-collapse">
-					<ul class="nav">
-					<li class="divider"></li>
-					<li class="dropdown offset7">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<?php echo "Anda login sebagai "; echo $_SESSION["username"];?>
-						<b class="caret"></b></a>
-					</li>
-					<li><a href="logout.php"><i class="icon-off"></i>&nbsp; &nbsp; &nbsp; Logout</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	<br/><br/><br/><br/><br/><br/><br/><br/><br/>
-	<div class="container">
-		<center><h1>Selamat Datang di ELISA Universitas Gadjah Mada</h1></center>
-	</div>
+
+  <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">E-Learning</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar-static-top/">Anda login sebagai <?php echo $_SESSION['username'];?></a></li>
+            <li class="active"><a href="logout.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h3>Selamat Datang di ELISA Universitas Gadjah Mada</h3>
+        <p>Mari berbagi pengetahuan, berkolaborasi dan menjadi produktif dalam kerangka pembelajaran.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Selengkapnya &raquo;</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
 </body>
--- soap/logout.php
+++ soap/logout.php
@@ -1,10 +1,10 @@
 <?php
-	//memulai session
-	session_start();
-	//cek  adanya  session,  jika  session  ada  maka  akan  di  unset dan  dilanjutkan  dengan session_destroy
-	if(ISSET($_SESSION['username'])){ 
-		UNSET($_SESSION['username']);
-	}
-	header("location: index.php");
-	session_destroy();
+//memulaisession
+session_start();
+//cek adanya session, jika session ada maka akan di unset dan dilanjutkan dengan session_destroy 
+if(ISSET($_SESSION['username'])){ 
+  UNSET($_SESSION['us ername']);
+}
+header("location: index.php"); 
+session_destroy();
 ?>
